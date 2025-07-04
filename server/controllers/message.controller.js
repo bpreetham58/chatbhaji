13b9e74baf006c466ae6f98d41816dcbfceb7798
Preetham B <bpreetham58@gmail.com>
2025-07-04 18:03:01 +0530

    started socket at 2.24

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.middleware.js";
 import { User } from "../models/user.model.js";
 import { Message } from "../models/message.model.js";
+import {v2 as cloudinary} from "cloudinary";
 
 export const getAllUsers = catchAsyncError(async (req, res, next) => {
     const user = req.user;
@@ -37,5 +38,28 @@ export const getMessages = catchAsyncError(async (req, res, next) => {
 });
 
 export const sendMessage = catchAsyncError(async (req, res, next) => {
+    const {text}=req.body;
+    const media=req?.files?.media;
+    const {id:receiverId} =req.params;
+    const senderId=req.user._id;
+
+    const receiver = await User.findById(receiverId);
+    if (!receiver) {
+        return res.status(400).json({
+            success: false,
+            message: "Receiver ID invalid",
+        });
+    }
+
+    const sanitizedtext=text?.trim() || "";
+    if(!sanitizedtext && !media){
+        return res.status(400).json({
+            success: false,
+            message: "Message cannot be empty",
+        });
+    }
+
+
+
 });
 
